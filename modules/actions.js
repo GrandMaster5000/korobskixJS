9f4d5ee603177d208e2ecb5f6a0678ecddcb500b
Tolik22869 <ggamer556@mail.ru>
2021-08-07 17:29:58 +0300

    rename actions.js to handler.js and add new actions.js

--- modules/actions.js
+++ modules/actions.js
@@ -1,43 +1,72 @@
 'use strict';
 
-import kor from '../core';
+import kor from "../core";
 
-kor.prototype.on = function (eventName, callback , ...options) {
-    if(!eventName || !callback) {
-        return this;
-    }
-    
+kor.prototype.html = function(content) {
     for (let i = 0; i < this.length; i++) {
-        this[i].addEventListener(eventName, callback, ...options);
+        if(content) {
+            this[i].innerHTML = content;
+        } else {
+            return this[i].innerHTML;
+        }
     }
 
     return this;
 };
 
+kor.prototype.eq = function(i) {
+   const swap = this[i];
+   const objLength = Object.keys(this).length;
 
-kor.prototype.off = function (eventName, callback , ...options) {
-    if(!eventName || !callback) {
-        return this;
-    }
-    for (let i = 0; i < this.length; i++) {
-        this[i].removeEventListener(eventName, callback, options);
-    }
+   for (let i = 0; i < objLength; i++) {
+        delete this[i];
+   }
+
+    this[0] = swap;
+    this.length = 1;
 
     return this;
 };
 
-kor.prototype.click = function (handler, ...options) {
-    for (let i = 0; i < this.length; i++) {
-        if(!this[i].addEventListener) {
+kor.prototype.index = function() {
+    const parent = this[0].parentNode;
+    const childs = [...parent.children];
+
+    const findMyIndex = (item) => {
+        return item == this[0];
+    };
+
+    return childs.findIndex(findMyIndex);
+};
+
+kor.prototype.find = function(selector) {
+    let numberOfItems = 0,
+    counter = 0;
+
+    const copyObj = Object.assign({}, this);
+
+    for (let i = 0;i < copyObj.length; i++) {
+        const arr = copyObj[i].querySelectorAll(selector);
+        if(arr.length == 0) {
             continue;
         }
 
-        if(handler) {
-            this[i].addEventListener('click', handler, ...options);
-        } else {
-             this[i].click();
-        }  
+        for (let j = 0; j < arr.length; j++) {
+            this[counter] = arr[j];
+            counter++;
+        }
+
+        numberOfItems += arr.length;
+    }
+
+    this.length = numberOfItems;
+
+    const objLength = Object.keys(this).length;
+    for (; numberOfItems < objLength; numberOfItems++) {
+        delete this[numberOfItems];
     }
 
     return this;
 };
+ 
+
